697dcce1ca74333319817f4befcb3a008ef19b38
Thibaud Colas <thibaudcolas@gmail.com>
2017-11-06 15:50:42 +0200

    Add trove classifier to convey Python 3 support

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
