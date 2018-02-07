51dfbc07853c2402274ec086f95db8f00b962f3a
Loic Teixeira <hello@lta.me>
2018-02-08 09:42:39 +1000

    Cleanup (#12)
    * Remove obsolete is_abstract
    
    As noted in Wagtail 1.1 release notes, this isn't needed when the class is abstract.
    http://docs.wagtail.io/en/v1.13.1/releases/1.1.html#is-abstract-flag-on-page-models-has-been-replaced-by-is-creatable
    
    * Refactor
    
    Dictionary iterate on keys.
    
    * Ignore E402 module level import not at top of file
    
    a790bcade9bf5544f81c1b0dbfe45161e34571e5 introduces conditional imports to support Wagtail 2.0
    which causes the linter to complain about the following imports not being at the top of the file.
    
    Ignoring the error on the following lines with `noqa` feels wrong as the error is not directly caused by it but by the imports above. Therefore ignoring all together for now. This can be removed one we drop support of Wagtail prior to 2.0.
    
    * Update changelog with changes from a790bca
    
    * Add Python 3.4 and 3.6 classifiers
    
    * Fix erroneous license classifier

--- wagtailcaptcha/models.py
+++ wagtailcaptcha/models.py
@@ -10,14 +10,12 @@ from .forms import WagtailCaptchaFormBuilder
 class WagtailCaptchaEmailForm(AbstractEmailForm):
     """Pages implementing a captcha form with email notification should inhert from this"""
 
-    is_abstract = True  # Don't display me in "Add"
-
     def __init__(self, *args, **kwargs):
         super(WagtailCaptchaEmailForm, self).__init__(*args, **kwargs)
         self.form_builder = WagtailCaptchaFormBuilder
 
     def process_form_submission(self, form):
-        if WagtailCaptchaFormBuilder.CAPTCHA_FIELD_NAME in form.fields.keys():
+        if WagtailCaptchaFormBuilder.CAPTCHA_FIELD_NAME in form.fields:
             form.fields.pop(WagtailCaptchaFormBuilder.CAPTCHA_FIELD_NAME)
         return super(WagtailCaptchaEmailForm, self).process_form_submission(form)
 
@@ -27,8 +25,6 @@ class WagtailCaptchaEmailForm(AbstractEmailForm):
 
 class WagtailCaptchaForm(AbstractForm):
     """Pages implementing a captcha form should inhert from this"""
-
-    is_abstract = True  # Don't display me in "Add"
 
     def __init__(self, *args, **kwargs):
         super(WagtailCaptchaForm, self).__init__(*args, **kwargs)
