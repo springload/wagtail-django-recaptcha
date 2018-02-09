ef79a43bf61206f50d04cbe9c659bbfefc76b317
Loic Teixeira <loic@springload.co.nz>
2018-02-09 12:39:16 +1000

    Add failing tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,20 +1,63 @@
 from __future__ import absolute_import, unicode_literals
 
+import json
+from test.test_support import EnvironmentVarGuard
+
 from django.test import TestCase
 
 from home.models import TestCaptchaEmailFormPage, TestCaptchaFormPage
 from wagtailcaptcha.forms import WagtailCaptchaFormBuilder
 
 
-class TestCaptchaEmailFormPageTestCase(TestCase):
+class CaptchaTestingModeMixin(TestCase):
+    """Allow Captcha to pass regardless of the value provided"""
+
+    def setUp(self):
+        self.captcha_testing_mode_env = EnvironmentVarGuard()
+        self.captcha_testing_mode_env.set('RECAPTCHA_TESTING', 'True')
+
+        self.captcha_form_data = {'recaptcha_response_field': 'PASSED'}
+
+
+class TestCaptchaEmailFormPageTestCase(CaptchaTestingModeMixin, TestCase):
+    fixtures = ['test_data.json']
+
     def test_captcha_form_builder_is_set(self):
         page = TestCaptchaEmailFormPage()
 
         self.assertIs(page.form_builder, WagtailCaptchaFormBuilder)
 
+    def test_captcha_field_is_removed_from_submission_data(self):
+        page = TestCaptchaEmailFormPage.objects.get(slug='email-form')
+        form_data = dict(self.captcha_form_data, name='Robert')
+        form = page.get_form(form_data)
 
-class TestCaptchaFormPageTestCase(TestCase):
+        with self.captcha_testing_mode_env:
+            self.assertTrue(form.is_valid())
+
+        form_submission = page.process_form_submission(form)
+        submission_data = json.loads(form_submission.form_data)
+
+        self.assertNotIn(WagtailCaptchaFormBuilder.CAPTCHA_FIELD_NAME, submission_data)
+
+
+class TestCaptchaFormPageTestCase(CaptchaTestingModeMixin, TestCase):
+    fixtures = ['test_data.json']
+
     def test_captcha_form_builder_is_set(self):
         page = TestCaptchaFormPage()
 
         self.assertIs(page.form_builder, WagtailCaptchaFormBuilder)
+
+    def test_captcha_field_is_removed_from_submission_data(self):
+        page = TestCaptchaFormPage.objects.get(slug='form')
+        form_data = dict(self.captcha_form_data, name='Robert')
+        form = page.get_form(form_data)
+
+        with self.captcha_testing_mode_env:
+            self.assertTrue(form.is_valid())
+
+        form_submission = page.process_form_submission(form)
+        submission_data = json.loads(form_submission.form_data)
+
+        self.assertNotIn(WagtailCaptchaFormBuilder.CAPTCHA_FIELD_NAME, submission_data)
