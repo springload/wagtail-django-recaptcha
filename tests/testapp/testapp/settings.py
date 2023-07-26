6f701acfa833988a012af784f4e68529c27a234e
Kat <katdom13@gmail.com>
2023-07-26 22:59:45 +0800

    Support/wagtail 41 (#2)
    * Update README for latest Wagtail versions (#25)
    
    * Updated tests and documentations
    
    * Fixed Wagtail version until 4.1 for this PR, updated django url to re_path
    
    * Ran updatemodulepaths, isort
    
    * Linting
    
    * Silence warnings
    
    * Added migration file from upgrade
    
    * Fix failing tests
    
    * Linting
    
    * Silence more warnings
    
    * Add github actions
    
    * Fixed .travis.yml to reflect updated tox.ini
    
    * Remove SessionAuthenticationMiddleware
    
    https://docs.djangoproject.com/en/2.0/releases/2.0/#miscellaneous
    
    ---------
    
    Co-authored-by: benoitvogel <benoit.vogel@gmail.com>
    Co-authored-by: Katherine Domingo <katherine.domingo@torchbox.com>
    Co-authored-by: Nick Moreton <nick.moreton@torchbox.com>

--- tests/testapp/testapp/settings.py
+++ tests/testapp/testapp/settings.py
@@ -14,99 +14,79 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
 # Application definition
-if wagtail.VERSION >= (2, 0):
-    wagtail_apps = [
-        'wagtail.contrib.forms',
-        'wagtail.sites',
-        'wagtail.users',
-        'wagtail.admin',
-        'wagtail.core',
-        'wagtail.documents',
-        'wagtail.images',
-    ]
-    wagtail_middlewares = [
-        'wagtail.core.middleware.SiteMiddleware',
-    ]
-else:
-    wagtail_apps = [
-        'wagtail.wagtailforms',
-        'wagtail.wagtailsites',
-        'wagtail.wagtailusers',
-        'wagtail.wagtaildocs',
-        'wagtail.wagtailimages',
-        'wagtail.wagtailadmin',
-        'wagtail.wagtailcore',
-    ]
-    wagtail_middlewares = [
-        'wagtail.wagtailcore.middleware.SiteMiddleware',
-    ]
+wagtail_apps = [
+    "wagtail.contrib.forms",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.admin",
+    "wagtail" if wagtail.VERSION >= (3, 0) else "wagtail.core",
+    "wagtail.documents",
+    "wagtail.images",
+]
+wagtail_middlewares = []
 
 INSTALLED_APPS = wagtail_apps + [
-    'home',
-
-    'captcha',
-    'wagtailcaptcha',
-
-    'modelcluster',
-    'taggit',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "home",
+    "captcha",
+    "wagtailcaptcha",
+    "modelcluster",
+    "taggit",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
 ] + wagtail_middlewares
 
-ROOT_URLCONF = 'testapp.urls'
+ROOT_URLCONF = "testapp.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(PROJECT_DIR, "templates"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'testapp.wsgi.application'
+WSGI_APPLICATION = "testapp.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'var', 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(PROJECT_DIR, "var", "db.sqlite3"),
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -118,36 +98,40 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'var', 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_DIR, "var", "media")
+MEDIA_URL = "/media/"
 
 
 # Wagtail settings
 
-WAGTAIL_SITE_NAME = 'testapp'
+WAGTAIL_SITE_NAME = "testapp"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = WAGTAILADMIN_BASE_URL = "http://example.com"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4*5e^@2%(h#$*b4=ze_kcdw46-$0z#rrf3661c5(&+x^oj=4)+'
+SECRET_KEY = "4*5e^@2%(h#$*b4=ze_kcdw46-$0z#rrf3661c5(&+x^oj=4)+"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Silence some errors
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 try:
     from .local import *  # noqa: F401, F403
--- wagtailcaptcha/__init__.py
+++ wagtailcaptcha/__init__.py
@@ -1,1 +1,3 @@
-__version__ = '1.0'
+from __future__ import absolute_import, unicode_literals
+
+__version__ = "1.0"
